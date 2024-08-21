eda90bb624144006ddab30a9a1ded92254578d65
Jym Cheong <jym1976+sftp@gmail.com>
2024-08-21 14:27:33 +0800

    deal with Standard Users logon

--- notify.ps1
+++ notify.ps1
@@ -73,35 +73,9 @@ $subscription.Enabled = $true
 Write-Log "Monitoring $logName for EventID $eventId. Press Ctrl+C to exit."
 
 try {
-    $i = 0
     while ($true) {
         Start-Sleep -Seconds 5
         Write-Log "$i Watcher state: $($subscription.Enabled)"
-        $i++
-        if($i -lt 60) { continue } # checking version every 5 minutes
-        $i = 0
-        try {
-            $webClient = New-Object System.Net.WebClient
-            $webClient.Encoding = [System.Text.Encoding]::UTF8
-            $webcontent = $webClient.DownloadString($versionUrl)
-            $folderName = "version" + $webContent.Trim()
-            if($folderName -ne "") {
-                # Check if the local folder exists
-                    if (Test-Path -Path $folderName -PathType Container) {
-                        Write-Host "'$folderName' is latest version."
-                    } else {
-                        Write-Host "Updating to '$folderName'..."
-                        Get-ChildItem -Directory -Filter ".\version*" -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
-                        New-Item -Path $folderName -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-                        # restart sch task
-                        schtasks /End /TN $taskName
-                        schtasks /Run /TN $taskName
-                    }
-            } 
-        }
-        catch {
-            Write-Host "something went wrong with web fetch... trying later!"
-        }
     }
 }
 finally {
---
+++ update.ps1
@@ -0,0 +1,31 @@
+# Define the event log and event ID you want to monitor
+
+$versionUrl = "https://ransomSHIELD.github.io//version.txt"
+$taskName = "ransomSHIELD"
+
+while ($true) {
+    Start-Sleep -Seconds 300
+    
+    try {
+        $webClient = New-Object System.Net.WebClient
+        $webClient.Encoding = [System.Text.Encoding]::UTF8
+        $webcontent = $webClient.DownloadString($versionUrl)
+        $folderName = "version" + $webContent.Trim()
+        if($folderName -ne "") {
+            # Check if the local folder exists
+                if (Test-Path -Path $folderName -PathType Container) {
+                    Write-Host "'$folderName' is latest version."
+                } else {
+                    Write-Host "Updating to '$folderName'..."
+                    Get-ChildItem -Directory -Filter ".\version*" -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+                    New-Item -Path $folderName -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+                    # restart sch task
+                    schtasks /End /TN $taskName
+                    schtasks /Run /TN $taskName
+                }
+        } 
+    }
+    catch {
+        Write-Host "something went wrong with web fetch... trying later!"
+    }
+}
